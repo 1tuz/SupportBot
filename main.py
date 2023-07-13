7bb96dca11bc4262ddc8c9f7adbeebf3f8d18138
Antony <58532577+1tuz@users.noreply.github.com>
2023-07-13 23:08:50 +0300

    Update main.py
    Исправил бесконечное ожидание коммента + сделал распознавание никнейма юзера + добавил валидацию номера

--- main.py
+++ main.py
@@ -37,14 +37,18 @@ async def process_name(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['name'] = message.text
     # Запрос номера телефона у пользователя
-    await message.answer("Отлично! Теперь введите ваш номер телефона:")
+    await message.answer("Отлично! Теперь введите ваш номер телефона, начиная с '+7':")
     await SupportForm.phone.set()
 
 
 @dp.message_handler(state=SupportForm.phone)
 async def process_phone(message: types.Message, state: FSMContext):
+    phone_number = message.text
+    if not phone_number.startswith('+7'):
+        await message.answer("Пожалуйста, введите номер телефона, начиная с '+7'. Попробуйте еще раз:")
+        return
     async with state.proxy() as data:
-        data['phone'] = message.text
+        data['phone'] = phone_number
     # Предложение выбрать одну из проблемных категорий
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
     keyboard.add("Проблемы с сайтом", "Проблемы с регистрацией/авторизацией", "Проблемы с оплатой", "Другое")
@@ -63,8 +67,14 @@ async def process_problem(message: types.Message, state: FSMContext):
     else:
         # Сразу завершаем диалог при выборе других категорий
         await state.finish()
-        await message.answer("Спасибо за предоставленную информацию и комментарий! "
-                             "Ваша заявка передана специалистам, которые займутся ей в ближайшее время!"
+        name = data['name']
+        phone = data['phone']
+        problem = data['problem']
+        username = "@" + message.from_user.username if message.from_user.username else ""
+        # Отправка собранной информации в отдельный канал
+        channel_id = os.getenv('SUPPORT_CHANNEL_ID')
+        await bot.send_message(channel_id, f"Имя: {name}\nТелефон: +7{phone}\nПроблема: {problem}\nНикнейм пользователя: {username}")
+        await message.answer("Спасибо за предоставленную информацию! Ваша заявка передана специалистам, которые займутся ей в ближайшее время!"
                              "\n\nЧтобы создать новую заявку, нажмите /start")
 
 
@@ -76,14 +86,14 @@ async def process_comment(message: types.Message, state: FSMContext):
         phone = data['phone']
         problem = data['problem']
         comment = data['comment']
+        username = "@" + message.from_user.username if message.from_user.username else ""
         # Отправка собранной информации в отдельный канал
         channel_id = os.getenv('SUPPORT_CHANNEL_ID')
-        await bot.send_message(channel_id, f"Имя: {name}\nТелефон: {phone}\nПроблема: {problem}\nКомментарий: {comment}")
+        await bot.send_message(channel_id, f"Имя: {name}\nТелефон: {phone}\nПроблема: {problem}\nКомментарий: {comment}\nНикнейм пользователя: {username}")
+        await message.answer("Спасибо за предоставленную информацию и комментарий! "
+                             "Ваша заявка передана специалистам, которые займутся ей в ближайшее время!"
+                             "\n\nЧтобы создать новую заявку, нажмите /start")
     await state.finish()
-    # Сообщение после завершения диалога
-    await message.answer("Спасибо за предоставленную информацию и комментарий! "
-                         "Ваша заявка передана специалистам, которые займутся ей в ближайшее время!"
-                         "\n\nЧтобы создать новую заявку, нажмите /start")
 
 
 @dp.message_handler(commands=['restart'])
